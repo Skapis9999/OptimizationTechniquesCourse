5b4577f3da0ff8ee64af4c5ae503dd962632f1ff
Skapis9999 <xskapetis@gmail.com>
2021-12-06 00:14:54 +0200

    Finishing Task 2

--- Task2/main.m
+++ Task2/main.m
@@ -15,6 +15,7 @@ clc
 %%Topic 1
 %3-D plot of the asking function
 
+figure(1)
 x = linspace(-2,2,20);
 y = x';
 z = (x.^3).*(exp((-x.^2)-(y.^4)));
@@ -25,14 +26,14 @@ surf(x,y,z)
 
 %%
 %%Initiating values
-x0 = [0 -1 1];
-y0 = [0 -1 1];
+x0 = [0, -1, 1];
+y0 = [0, -1, 1];
 epsilon = 0.001;
 
 %% 
 %%Method
 % d = gradientDescentFixed(epsilon, 1,1)
-[d2, xs, ys] = gradientDescent(epsilon, -1,1);
+[d2, xs, ys] = gradientDescent(epsilon, x0(1), y0(1));
 % d3 = gradientDescentArmijo(epsilon, 1,1)
 figure(2)
 surf(x,y,z)
