9d5d32ca6f048560321727bfb09f0b9303396dc9
Skapis9999 <xskapetis@gmail.com>
2021-12-06 16:53:04 +0200

    Fixed LM Method added

---
+++ Task2/2.4/LevenbergMarquardtFixed.m
@@ -0,0 +1,31 @@
+function [results,xs,ys] = LevenbergMarquardtFixed(e,x,y)
+k=1;
+results = [f(x,y)];
+gammaKappa = 0.3;
+xs = [];
+ys = [];
+
+d = grad(x,y);
+while norm(d) >= e
+    
+    h = hessianMatrix(x,y);
+    m = 0; %m0 = 0
+    pM = h + m*eye(size(h)); %positive defined Matrix
+    if pM(1,1)<=0 || pM(1,1)*pM(2,2)-pM(1,2)*pM(2,1)<=0
+        m = m+1;
+        pM = h+m*eye(size(h));
+        if m > 1000
+            disp('!!The hessian is not positive definite for any m-- Error!!')
+            break
+    end
+    dk = hessianMatrix(x,y)/pM
+    x = x  - gammaKappa*dk(1);
+    y = y  - gammaKappa*dk(2);
+    k = k+1;
+    results = [results, f(x,y)];
+    d = grad(x,y); % d  for the next loop with new x and new y
+    xs = [xs, x];
+    ys = [ys, y];
+end
+end
+
---
+++ Task2/2.4/main.m
@@ -0,0 +1,39 @@
+%%Clear Everything
+clearvars
+clc
+
+%%
+%%Info
+%Skapetis Christos
+%AEM 9378
+%skapetis@ece.auth.gr
+%4th Task main faction
+%%
+%%Initiating values
+x0 = [0, -1, 1];
+y0 = [0, -1, 1];
+epsilon = 0.001;
+
+%%
+%%Method
+
+[d, xs, ys] = LevenbergMarquardtFixed(epsilon, x0(2), y0(2));
+
+x = linspace(-2,2,20);
+y = x';
+z = (x.^3).*(exp((-x.^2)-(y.^4)));
+
+figure(1)
+surf(x,y,z)
+hold on
+scatter3(xs,ys,(xs.^3).*(exp((-xs.^2)-(ys.^4))),'+','r')
+hold on 
+scatter3(xs(end),ys(end),(xs(end)^3)*(exp((-xs(end)^2)-(ys(end)^4))),'p','y')
+
+figure(2)
+clf
+fcontour(@(x,y) (x^3)*(exp((-x^2)-(y^4))),[-2, 2])
+hold on
+scatter(xs,ys,'+','r')
+hold on 
+scatter(xs(end),ys(end),'p','k')
