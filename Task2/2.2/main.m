781c7ada362e436e8a8d6a5f13137191127d1bd0
Skapis9999 <xskapetis@gmail.com>
2021-12-06 21:12:16 +0200

    Figures for Gradient Descent Method

--- Task2/2.2/main.m
+++ Task2/2.2/main.m
@@ -15,14 +15,18 @@ clc
 %%Topic 1
 %3-D plot of the asking function
 
-figure(1)
+% figure(1)
+% clf
 x = linspace(-2,2,20);
 y = x';
 z = (x.^3).*(exp((-x.^2)-(y.^4)));
-surf(x,y,z)
+% surf(x,y,z)
+% legend('z','dot')
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% title('3-D plot of z = $x^{3} * e^{-x^{2} - y^{4}}$ ','Interpreter','latex')
 
-% [zx,zy] = gradient(z,0.2);
-% delta = [zx,zy]
 
 %%
 %%Initiating values
@@ -32,15 +36,26 @@ epsilon = 0.001;
 
 %% 
 %%Method
-% [~, xs, ys] = gradientDescentFixed(epsilon, 1,1)
-% [~, xs, ys] = gradientDescent(epsilon, x0(2), y0(2));
-[~, xs, ys] = gradientDescentArmijo(epsilon, x0(3), y0(3))
+i = 2; %x0 that we choose
+% [~, xs, ys] = gradientDescentFixed(epsilon, x0(i), y0(i))
+% [~, xs, ys] = gradientDescent(epsilon, x0(i), y0(i));
+[~, xs, ys] = gradientDescentArmijo(epsilon, x0(i), y0(i))
 figure(2)
+clf
 surf(x,y,z)
 hold on
 scatter3(xs,ys,(xs.^3).*(exp((-xs.^2)-(ys.^4))),'+','r')
 hold on 
 scatter3(xs(end),ys(end),(xs(end)^3)*(exp((-xs(end)^2)-(ys(end)^4))),'p','y')
+legend('z','steps','final solution')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+% title('z = $x^{3} * e^{-x^{2} - y^{4}}$ with steps for Gradient Descent Method for gamma = 1 for (1,1)','Interpreter','latex')
+% title('z = $x^{3} * e^{-x^{2} - y^{4}}$ with steps for Gradient Descent Method best gamma for (0,0)','Interpreter','latex')
+title('z = $x^{3} * e^{-x^{2} - y^{4}}$ with steps for Gradient Descent Method Armijo gamma for (-1,-1)','Interpreter','latex')
+
+
 
 figure(3)
 clf
@@ -49,3 +64,7 @@ hold on
 scatter(xs,ys,'+','r')
 hold on 
 scatter(xs(end),ys(end),'p','k')
+legend('z','steps','final solution')
+xlabel('x')
+ylabel('y')
+title('z = $x^{3} * e^{-x^{2} - y^{4}}$ with steps for Gradient Descent Method Armijo gamma for (-1,-1)','Interpreter','latex')
